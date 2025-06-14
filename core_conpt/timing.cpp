20103c2b0d0838874689bbd193c43db508fd6136
Arunava Basu <arunavabasudev@gmail.com>
2025-06-14 13:01:46 +0530

    feat: addup mmi

--- core_conpt/timing.cpp
+++ core_conpt/timing.cpp
@@ -2,12 +2,36 @@
 #include <chrono>
 #include <thread>
 using namespace std;
+using namespace std::chrono;
 
-int main(){
+struct Timer {
+    time_point<system_clock> start, end;
+    duration<float> dur;
+    Timer() {
+        start = high_resolution_clock::now();
+    }
+    ~Timer() {
+        end = high_resolution_clock::now();
+        dur = end - start;
+        cout << (dur.count() * 1000.0F) <<"ms";
+
+    }
+};
+void fn() {
+    Timer timer;
+    for (int i = 0; i < 100;++i) {
+        // cout << "hello" << endl; // slow
+        cout << "hello\n"; //fast
+    }
+}
+int main() {
+    /*
     auto start = chrono::high_resolution_clock::now();
     this_thread::sleep_for(1s);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<float> duration = end - start;
     cout << duration.count() << "s" << endl;
+    */
+    fn();
     return 0;
 }
--- number_theory/modular_multiplicative_inverse.cpp
+++ number_theory/modular_multiplicative_inverse.cpp
@@ -1,7 +1,6 @@
 /**
  * Modular Multiplicative Inverse(19)
 */
-#include <io_setup.h>
 #include <bits/stdc++.h>
 using namespace std;
 const int MOD = 1e9 + 7;
@@ -20,7 +19,7 @@ int modulerInverse(int a, long long b, int m) {
 /**
  * There are N children and K toffees.K < N, Count the number of ways to distribute toffee among N students such that each student get 1 toffee->nCk
  * N < 10^9, K<N<10^9 , %M , M=10^9+7
- * for Q queries also Q<10^5 
+ * for Q queries also Q<10^5
  * n!/((n-r)!*r!)
 */
 
@@ -37,8 +36,8 @@ int main() {
      * (a%M * b%M)%M =1
      * we can find MMI only is a and M is co-prime.
 */
-    // cout << inverse(n, MOD - 2, MOD);
-    fact[0] = 1;
+    // cout << inverse(n, MOD - 2, MOD); 
+    fact[0] = 1; 
     for (int i = 1; i < N;i++) {
         fact[i] = (fact[i - 1] * 1LL * i) % MOD;
     }
